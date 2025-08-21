dda7ad6125b7acd333237ea3ff092fa6a1f8baa0
bruce minanga <bruceminanga@gmail.com>
2025-08-22 00:33:14 +0300

    fixed: kubernetes errors

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost", cast=Csv())
+ALLOWED_HOSTS = config(
+    "DJANGO_ALLOWED_HOSTS", default="127.0.0.1,localhost", cast=Csv()
+)
 
 
 # Application definition
